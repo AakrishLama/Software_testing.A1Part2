a9629fd1151abc869aedb22bf2029eb308e4a0fe
Aakrish Lama <al226na@student.lnu.se>
2025-12-07 10:10:11 +0100

    GREEN: Implement fine for user logic

--- app/src/main/java/org/example/model/Member.java
+++ app/src/main/java/org/example/model/Member.java
@@ -8,12 +8,14 @@ public class Member {
     private final String name;
     private final String email;
     private final List<String> borrowedBooks;
+    private double totalFines;
 
     public Member(String memberId, String name, String email) {
         this.memberId = memberId;
         this.name = name;
         this.email = email;
         this.borrowedBooks = new ArrayList<>();
+        this.totalFines = 0.0;
     }
 
     public String getMemberId() {
@@ -38,6 +40,18 @@ public class Member {
 
     public void removeBorrowedBook(String isbn) {
         borrowedBooks.remove(isbn);
+    }
+
+    public void addFines(double amount) {
+        totalFines += amount;
+    }
+
+    public void removeFines(double amount) {
+        totalFines -= amount;
+    }
+
+    public double getTotalFines() {
+        return totalFines;
     }
 
 }
