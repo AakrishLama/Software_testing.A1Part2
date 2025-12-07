fcc7d8bc2d71452c449e336f4047585234af6624
Aakrish Lama <al226na@student.lnu.se>
2025-12-07 23:15:55 +0100

    GREEN: simple implementation for contstructtor

--- app/src/main/java/org/example/service/FineCalculator.java
+++ app/src/main/java/org/example/service/FineCalculator.java
@@ -1,5 +1,10 @@
 package org.example.service;
 
+import java.time.LocalDate;
+
 public class FineCalculator {
 
+    public double calculateFine(LocalDate dueDate, LocalDate returnDate) {
+        return 0.0;
+    }
 }
