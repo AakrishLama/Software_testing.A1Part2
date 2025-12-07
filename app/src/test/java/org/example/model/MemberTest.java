63f0314897cbaabfb55fec40f2f6351ebdc453e6
Aakrish Lama <al226na@student.lnu.se>
2025-12-07 09:26:14 +0100

    RED: Test for initial borrowed books list

--- app/src/test/java/org/example/model/MemberTest.java
+++ app/src/test/java/org/example/model/MemberTest.java
@@ -18,4 +18,13 @@ public class MemberTest{
         assertEquals(name, member.getName());
         assertEquals(email, member.getEmail());
     }
-}
+
+    @Test
+    public void testInitialBorrowedBookIsEmpty(){
+        Member member = new Member("M001", "John Doe", "john@example.com");
+
+        // check if the member has no borrowed books
+        assertNotNull(member.getBorrowedBooks());
+        assertTrue(member.getBorrowedBooks().isEmpty());
+    }
+}  
