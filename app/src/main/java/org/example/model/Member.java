ef7ff0f237c279c860d31f5b4793663955208156
Aakrish Lama <al226na@student.lnu.se>
2025-12-07 09:19:18 +0100

    RED: test for basic member creation

---
+++ app/src/main/java/org/example/model/Member.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Member{
+    public Member(String memberId, String name, String email){
+        //todo 
+    }
+    
+    public String getMemberId(){
+        return null;
+    }
+    
+    public String getName(){
+        return null;
+    }
+    
+    public String getEmail(){
+        return null;
+    }
+    
+}
---
+++ app/src/test/java/org/example/model/MemberTest.java
@@ -0,0 +1,21 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemberTest{
+
+    @Test
+    public void testMemberCreation(){
+        // hard coded
+        String memberId = "M001";
+        String name = "John Doe";
+        String email = "john@example.com";
+
+        Member member = new Member(memberId, name, email);
+
+        assertEquals(memberId, member.getMemberId());
+        assertEquals(name, member.getName());
+        assertEquals(email, member.getEmail());
+    }
+}
