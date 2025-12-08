83a49705aaa959a471ffc596de0bb9cb6832ff3c
ekrlstd <ebbekarlstad@proton.me>
2025-12-08 15:12:14 +0100

    RED: set up initial test file

---
+++ app/src/test/java/org/example/model/BookTest.java
@@ -0,0 +1,21 @@
+package org.example.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+public class BookTest {
+    private Book book;
+    // final static makes it a constant so can't be reinitialized
+    private static final String ISBN = "978-3-16-138410-0";
+    private static final String TITLE = "Blood Meridian";
+    private static final String AUTHOR = "Cormac McCarthy";
+    private static final boolean AVAILABLE = true;
+    private static final String BORROWEDBY = "John Doe";
+
+    @BeforeEach
+    public void setUp() {
+        book = new Book(ISBN, TITLE, AUTHOR);
+    }
+}
