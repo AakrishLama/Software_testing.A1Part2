6d9ea096ddaa72e723bca4f3e011c0cc467518ef
Aakrish Lama <al226na@student.lnu.se>
2025-12-08 14:58:51 +0100

    RED: dependency injection in constructor

--- app/src/test/java/org/example/service/NotificationServiceTest.java
+++ app/src/test/java/org/example/service/NotificationServiceTest.java
@@ -2,11 +2,20 @@ package org.example.service;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+import org.example.model.Member;
 
 public class NotificationServiceTest {
     @Test
     public void testNotificationServiceCreation() {
         NotificationService notificationService = new NotificationService();
         assertNotNull(notificationService);
+    }
+
+    @Test
+    public void testSendBorrowConfirmation() {
+        NotificationService service = new NotificationService();
+        Member member = new Member("John", "john@example.com", "1234567890");
+        String bookTitle = "book";
+        service.sendBorrowConfirmation(member, bookTitle);
     }
 }
