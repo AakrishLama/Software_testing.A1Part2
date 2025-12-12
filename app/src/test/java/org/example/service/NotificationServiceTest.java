451429d7b80c2ad196224880bc2d805a01f319a5
ekrlstd <ebbekarlstad@proton.me>
2025-12-12 17:47:06 +0100

    GREEN: borrow confirm works

--- app/src/test/java/org/example/service/NotificationServiceTest.java
+++ app/src/test/java/org/example/service/NotificationServiceTest.java
@@ -1,6 +1,10 @@
 package org.example.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import org.example.model.Book;
 import org.example.model.Member;
@@ -17,14 +21,23 @@ public class NotificationServiceTest {
     public void setUp() {
         ns = new NotificationService();
         member = new Member("001", "Jane Doe", "janedoe@gmail.com");
-        book = new Book("978-3-16-138410-0", "To kill a Mockingbird", "Harper Lee");
+        book = new Book("978-3-16-138410-0", "To Kill a Mockingbird", "Harper Lee");
         fc = new FineCalculator();
     }
 
     @Test
     void testBorrowConfirmation() {
-        assertEquals(
-                "Successfully borrowed book.",
-                ns.sendBorrowConfirmation(member, book.getTitle()));
+        // all confirmation will get printed to we have to get createive with tests,
+        // reading the stdout
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        // use params of notification service
+        ns.sendBorrowConfirmation(member, book.getTitle());
+
+        String output = outputStream.toString();
+        assertEquals("Successfully borrowed book.", ns.sendBorrowConfirmation(member, book.getTitle()));
+
+        System.setOut(System.out); // restore normal output
     }
 }
