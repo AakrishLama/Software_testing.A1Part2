3ac56742440443b1c012e4f08b9d0b1ebe9f1678
ekrlstd <ebbekarlstad@proton.me>
2025-12-18 01:36:34 +0100

    REFACTOR: remove unused impoirt

--- app/src/test/java/org/example/ui/ConsoleUITest.java
+++ app/src/test/java/org/example/ui/ConsoleUITest.java
@@ -1,7 +1,6 @@
 package org.example.ui;
 
 import org.example.service.LibraryService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
